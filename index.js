8d43671c1114a7d140e5f628ee9c428a095ff6d7
nosirbek030381 <ismoilovn899@gmai.com>
2023-12-15 09:25:06 +0500

    1123

--- index.js
+++ index.js
@@ -10,13 +10,13 @@ const app = express();
 // SHAARRTT
 const hbs = create({ defaultLayout: 'main', extname: 'hbs' });
 // Set Handlebars as the view engine
-app.engine('hbs', hbs.engine());
+app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 app.set('views', './views');
 
+app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(Routes);
